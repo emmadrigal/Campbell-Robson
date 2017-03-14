de32074b1e1ad21db9edea43a3775ccd5b53552b
Emmanuel Madrigal <emma.madri97@gmail.com>
2017-03-14 11:44:21 -0600

    Added necessary function skeleton to the c++ file

---
+++ carta.cpp
@@ -0,0 +1,61 @@
+
+
+/**
+ * Returns the value for a descrete sine Function that increases its frecuency linearly
+ * It always returns a value between 0 and 1
+ * @param pX value of the x pixel value TODO check which is going to be the max value for the pixel
+ * @return The value of the function
+ */
+float discreteSine(int pX){
+    //TODO Find and implement function
+    return 0;
+}
+
+/**
+ * Returns the value of amplitud for a function, this value begins from 0 and goes up to 127
+ * @param pX value of the x pixel value TODO check which is going to be the max value for the pixel
+ * @return amplitud for the function
+ */
+int amplitudValue(int pX){
+    //TODO Find and implement function
+    return 0;
+}
+
+/**
+ * Returns scaled value for x when evaluating a logarithmic function
+ * @param pX value of the x pixel value TODO check which is going to be the max value for the pixel
+ * @return scaled value for X
+ */
+int scalingFunction(int pX){
+    //TODO implement function
+    
+    //TODO verify that constants return correct values
+}
+
+
+/**
+ * Displays a Campbell-Robson chart
+ * @param cols Number of colums which the image will posess
+ * @param rows Number of rows which the image will posees
+ */
+void displayLinearChart(int cols = 512, int rows = 512, bool linear){
+    //TODO implement display of linear chart
+    
+    //TODO implement display of logarithmic
+}
+
+
+/**
+ * Displays a single line of a Cambell-Robson chart
+ * @param cols Number of colums which the image will posess
+ * @param rows Number of rows which the image will posees
+ */
+void displaySingleLine(int row, int numCols = 512, bool linear){{
+    //TODO implement display
+}
+
+
+
+int main(int ac, char* av[]){
+    return 0;      
+}
